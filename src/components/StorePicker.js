14e6b6fb267667e8d7cd1e71e6cfd6d17a8295f6
Hunter Stevens <onebree@gmail.com>
2016-11-10 17:05:33 -0500

    Add an onSubmit event and reference to input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,23 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  render(){
+  goToStore(event) {
+    event.preventDefault();
+
+    console.log("You changed the URL");
+
+    // First grab the text from the box
+    console.log(this.storeInput.value)
+
+    // Second, trasnition from / to /store/:storeId
+  }
+
+  render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store Name" defaultValue={
+          getFunName()} ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit store</button>
       </form>
     )
