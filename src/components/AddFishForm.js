e8edd493f7491c18d03e5f7144e20b60ace73a01
Hunter Stevens <onebree@gmail.com>
2016-11-11 13:42:14 -0500

    Update fishes state by creating fish in form

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -13,10 +13,14 @@ class AddFishForm extends React.Component {
       image: this.image.value
     }
 
+    this.props.addFish(fish);
+
+    this.fishForm.reset();
   }
+
   render() {
     return (
-      <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+      <form ref={(input) => this.fishForm = input } className="fish-edit" onSubmit={(e) => this.createFish(e)}>
         <input ref={(input) => this.name = input} type="text" placeholder="Fish Name" />
         <input ref={(input) => this.price = input} type="text" placeholder="Fish Price" />
         <select ref={(input) => this.status = input}>
@@ -25,7 +29,6 @@ class AddFishForm extends React.Component {
         </select>
         <textarea ref={(input) => this.desc = input} placeholder="Fish Desc"></textarea>
         <input ref={(input) => this.image = input} type="text" placeholder="Fish Image" />
-
         <button type="submit">+ Add Item</button>
       </form>
     )
